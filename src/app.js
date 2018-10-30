d409fec0f7f071b69ac5631ea733913d86521204
hunterbecton <hunter@hunterbecton.com>
2018-10-30 12:16:20 -0400

    Remove dummy edpense data

--- src/app.js
+++ src/app.js
@@ -14,13 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas Bill', createdAt: 1000}));
-//store.dispatch(addExpense({description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
     //Store is set above in a const
     <Provider store={store}>
