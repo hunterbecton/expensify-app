f401e4b9b53e5dd1879c7d39625e97f6752b6270
hunterbecton <hunter@hunterbecton.com>
2018-11-03 11:39:14 -0400

    Set startremoveexpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,15 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+// Remove expenses from firebase
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -45,6 +54,7 @@ export const setExpenses = (expenses) => ({
   expenses
 });
 
+// Load expenses from firebase
 export const startSetExpenses = () => {
   return (dispatch) => {
     return database.ref('expenses').once('value').then((snapshot) => {
